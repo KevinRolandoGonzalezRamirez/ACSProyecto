81a7b271fd8d15a1e32491ae69270ed9fd79685e
KevinRolandoGonzalezRamirez <kgonzalezr13@miumg.edu.gt>
2022-09-20 19:30:10 -0600

    ya está estan todas las invocaciones en program.cs

--- ApiRestSQL/Program.cs
+++ ApiRestSQL/Program.cs
@@ -12,7 +12,11 @@ builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<ILigaRepository, LigaRepository>();
 builder.Services.AddScoped<ITarjetaRepository, TarjetaRepository>();
+builder.Services.AddScoped<ILigaUsuarioRepository, LigaUsuarioRepository>();
 builder.Services.AddScoped<IPagoRepository, PagoRepository>();
+builder.Services.AddScoped<IEquipoRepository, EquipoRepository>();
+builder.Services.AddScoped<IEstadioRepository, EstadioRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
