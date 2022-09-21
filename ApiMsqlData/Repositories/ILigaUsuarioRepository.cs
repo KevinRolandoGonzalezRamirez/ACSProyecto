00f9efb6d45b2ee370ff43677af7515e3c3cfd2c
KevinRolandoGonzalezRamirez <kgonzalezr13@miumg.edu.gt>
2022-09-20 18:30:21 -0600

    Api realizada de LigaUsuario

---
+++ ApiMsqlData/Repositories/ILigaUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApiMsqlModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiMsqlData.Repositories
+{
+    public interface ILigaUsuarioRepository
+    {
+        Task<IEnumerable<ligaUsuario>> GetAllLigaUsuarios();//consulta general
+        Task<ligaUsuario> GetLigaUsuarioDetails(int id);//consulta específica
+        Task<bool> InsertLigaUsuario(ligaUsuario lig);//insertar
+        Task<bool> UpdateLigaUsuario(ligaUsuario lig);//update
+        Task<bool> DeleteLigaUsuario(ligaUsuario lig);//borrar
+    }
+}
